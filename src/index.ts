828a5efc0fe0f28d1a4f44353d6daab122802af4
Wolfgang Zolleis <wzolleis@web.de>
2018-03-25 23:37:00 +0200

    working sample for async select

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from "express";
 import methodOverride from "method-override";
 import logger from "morgan";
-import { getCleaner } from "./cleaner/cleaner_api";
+import { findAllCleaners } from "./cleaner/cleaner_api";
 
 const app = express();
 
@@ -12,7 +12,10 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
-app.get("/cc/api/cleaner", (req, res) => res.send(getCleaner()));
+app.get("/cc/api/cleaner", async (req: any, res: any) => {
+    const data = await findAllCleaners();
+    res.send(data);
+});
 
 // tslint:disable-next-line:no-console
 app.listen(process.env.PORT || 8080, () => console.log("Example app listening on port 8080!"));
